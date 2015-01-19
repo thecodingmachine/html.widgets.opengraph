89682e0c4dab2856dd502a02741cb48fedcf1044
Benoit <b.ngo@thecodingmachine.com>
2015-01-19 18:11:18 +0100

    First commit for the mouf opengraph support

---
+++ src/Mouf/Html/Widgets/Opengraph/HtmlOpenGraphImageWidget.php
@@ -0,0 +1,121 @@
+<?php
+namespace Mouf\Html\Widgets\BaseHtmlWidget;
+use Mouf\Html\HtmlElement\HtmlElementInterface;
+/**
+ * 
+ * @author benoit
+ * TODO: add getter
+ */
+class HtmlOpenGraphImageWidget implements HtmlElementInterface{
+	/**
+	 * 
+	 * @var string
+	 */
+	private $url = null;
+	/**
+	 * 
+	 * @var string
+	 */
+	private $secure_url = null;
+	/**
+	 * 
+	 * @var string
+	 */
+	private $type = null;
+	/**
+	 * 
+	 * @var int
+	 */
+	private $width = -1;
+	/**
+	 * 
+	 * @var int
+	 */
+	private $height = -1;
+	/**
+	 * 
+	 * @var bool
+	 */
+	private $hasChange = true;
+	/**
+	 * 
+	 * @var string
+	 */
+	private $html = "";
+	/**
+	 * 
+	 * @param string $url
+	 * @param string $secure_url
+	 * @param string $type
+	 * @param int $width
+	 * @param int $height
+	 */
+	function __construct($url, $secure_url = null, $type = null, $width = -1, $height = -1) {
+		$this->url = $url;
+		$this->secure_url = $secure_url;
+		$this->type = $type;
+		$this->width = $width;
+		$this->height = $height;
+		$this->hasChange = true;
+		$this->html = "";
+	}
+	/**
+	 * 
+	 * @param string $url
+	 */
+	public function setUrl($url) {
+		$this->url = $url;
+		$this->hasChange = true;
+	}
+	/**
+	 * 
+	 * @param string $url
+	 */
+	public function setSecureUrl($url) {
+		$this->secure_url = $url;
+		$this->hasChange = true;
+	}
+	/**
+	 * 
+	 * @param string $type
+	 */
+	public function setType($type) {
+		$this->type = $type;
+		$this->hasChange = true;
+	}
+	/**
+	 * 
+	 * @param int $width
+	 */
+	public function setWidth($width) {
+		$this->width = $width;
+		$this->hasChange = true;
+	}
+	/**
+	 * 
+	 * @param int $height
+	 */
+	public function setHeight($height) {
+		$this->height = $height;
+		$this->hasChange = true;
+	}
+	
+	private function constructHtml() {
+		$this->hasChange = false;
+		$html = "";
+		if ($this->url === null)
+			return $html;
+		$html .= '<meta property="og:image" content="'.$this->url.'" />';
+		$html .= ($this->secure_url === null ? '' : '<meta property="og:image:secure_url" content="'.$this->secure_url.'" />');
+		$html .= ($this->type === null ? '' : '<meta property="og:image:type" content="'.$this->type.'" />');
+		$html .= ($this->width === -1 ? '' : '<meta property="og:image:width" content="'.$this->width.'" />');
+		$html .= ($this->height === -1 ? '' : '<meta property="og:image:height" content="'.$this->height.'" />');
+		return $html;
+	}
+	
+	public function toHtml() {
+		if ($this->hasChange == true)
+			$this->html = $this->constructHtml();
+		return $this->html;
+	}
+}
---
+++ src/Mouf/Html/Widgets/Opengraph/HtmlOpenGraphWidget.php
@@ -0,0 +1,124 @@
+<?php
+namespace Mouf\Html\Widgets\BaseHtmlWidget;
+use Mouf\Html\HtmlElement\HtmlElementInterface;
+/**
+ * 
+ * @author benoit
+ * TODO add support of type: @see http://ogp.me/ and @see http://www.yakaferci.com/open-graph/
+ * TODO: add getter
+ */
+class HtmlOpenGraphWidget implements HtmlElementInterface{
+	/**
+	 * 
+	 * @var string 
+	 */
+	private $title;
+	/**
+	 * 
+	 * @var string 
+	 */
+	private $type;
+	/**
+	 * 
+	 * @var string
+	 */
+	private $url;
+	
+	/**
+	 * 
+	 * @var HtmlOpenGraphImageWidget
+	 */
+	private $imageObject;
+	
+	/**Optionnal metadata**/
+	/**
+	 * 
+	 * @var string
+	 */
+	private $audio;
+	/**
+	 * Short description
+	 * @var string
+	 */
+	private $description;
+	/**
+	 *  An enum of (a, an, the, "", auto)
+	 * @var string
+	 */
+	private $determiner;
+	/**
+	 *  format language_TERRITORY
+	 * @var string
+	 */
+	private $locale;
+	/**
+	 * 
+	 * @var string[]
+	 */
+	private $locale_alternate;
+	/**
+	 * 
+	 * @var string
+	 */
+	private $site_name;
+	/**
+	 * 
+	 * @var string
+	 */
+	private $video;
+	/**
+	 * TODO: add support of video
+	 * TODO: add support of music
+	 */
+	function __construct($title, $type, $url, $imageObject, $audio = null, $description = null, $determiner = null, $local = null, $local_alternate = null, $site_name = null, $video = null) {
+		$this->title = $title;
+		$this->type = $type;
+		$this->url = $url;
+		$this->imageObject = $imageObject;
+		
+		$this->audio = $audio;
+		$this->description = $description;
+		$this->determiner = $determiner;
+		$this->locale = $local;
+		$this->locale_alternate = $local_alternate;
+		$this->site_name = $site_name;
+		$this->video = $video;
+	}
+	
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+	public function setType($type) {
+		$this->type = $type;
+	}
+	public function setUrl($url) {
+		$this->url = $url;
+	}
+	public function setImageObject($imageObject) {
+		$this->imageObject = $imageObject;
+	}
+	
+	private function getOptionnalMetaData() {
+		$html = "";
+		$html .= $this->audio === null ? "" : '<meta property="og:audio" content="'.$this->audio.'" />';
+		$html .= $this->description === null ? "" : '<meta property="og:description" content="'.$this->description.'" />';
+		$html .= $this->determiner === null ? "" : '<meta property="og:determiner" content="'.$this->determiner.'" />';
+		$html .= $this->locale === null ? "" : '<meta property="og:locale" content="'.$this->locale.'" />';
+		foreach ($this->locale_alternate as $loc_alt) {
+			$html .= ($loc_alt === null || empty($loc_alt)) ? "" : '<meta property="og:locale_alternate" content="'.$loc_alt.'" />';
+		}
+		$html .= $this->site_name === null ? "" : '<meta property="og:site_name" content="'.$this->site_name.'" />';
+		$html .= $this->video === null ? "" : '<meta property="og:video" content="'.$this->video.'" />';
+		return $html;
+	}
+	
+	public function toHtml() {
+		$html = "";
+		$html .= '<meta property="og:title" content="'.$this->title.'" />
+				<meta property="og:type" content="'.$this->type.'" />
+				<meta property="og:url" content="'.$this->url.'" />';
+		$html .= $this->getOptionnalMetaData();
+		$html .= $this->imageObject->toHtml();
+		return $html;
+	}
+}
